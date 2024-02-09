c1bea7d9dab32eed242427ee6ed5234ba2ba2a1c
pableras172 <pableras172@hotmail.com>
2024-02-09 14:26:01 +0100

    Ajustes

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -27,6 +27,27 @@
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+
+                <div x-data="window.themeSwitcher()" x-init="switchTheme()" @keydown.window.tab="switchOn = false" class="flex items-center justify-center space-x-2">
+                    <input id="thisId" type="checkbox" name="switch" class="hidden" :checked="switchOn">
+                
+                    <button 
+                        x-ref="switchButton"
+                        type="button" 
+                        @click="switchOn = ! switchOn; switchTheme()"
+                        :class="switchOn ? 'bg-blue-600' : 'bg-neutral-200'" 
+                        class="relative inline-flex h-6 py-0.5 ml-4 focus:outline-none rounded-full w-10">
+                        <span :class="switchOn ? 'translate-x-[18px]' : 'translate-x-0.5'" class="w-5 h-5 duration-200 ease-in-out bg-white rounded-full shadow-md"></span>
+                    </button>
+                
+                    <label @click="$refs.switchButton.click(); $refs.switchButton.focus()" :id="$id('switch')" 
+                        :class="{ 'text-blue-600': switchOn, 'text-gray-400': ! switchOn }"
+                        class="text-sm select-none">
+                        Dark Mode
+                    </label>
+                </div>
+
+
                 <!-- Teams Dropdown -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="ms-3 relative">
@@ -142,8 +163,8 @@
                     </x-dropdown>
                 </div>
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+            <div class="-mr-2 flex items-center sm:hidden">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -162,9 +183,29 @@
         </div>
 
 
-
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+           
+            <div x-data="window.themeSwitcher()" x-init="switchTheme()" @keydown.window.tab="switchOn = false" class="flex items-center justify-center space-x-2">
+                <input id="thisId" type="checkbox" name="switch" class="hidden" :checked="switchOn">
+            
+                <button 
+                    x-ref="switchButton"
+                    type="button" 
+                    @click="switchOn = ! switchOn; switchTheme()"
+                    :class="switchOn ? 'bg-blue-600' : 'bg-neutral-200'" 
+                    class="relative inline-flex h-6 py-0.5 ml-4 focus:outline-none rounded-full w-10">
+                    <span :class="switchOn ? 'translate-x-[18px]' : 'translate-x-0.5'" class="w-5 h-5 duration-200 ease-in-out bg-white rounded-full shadow-md"></span>
+                </button>
+            
+                <label @click="$refs.switchButton.click(); $refs.switchButton.focus()" :id="$id('switch')" 
+                    :class="{ 'text-blue-600': switchOn, 'text-gray-400': ! switchOn }"
+                    class="text-sm select-none">
+                    Dark Mode
+                </label>
+            </div>
+
+
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="shrink-0 me-3">
