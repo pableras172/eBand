512d04970c00ca6f0feb139f81b17eb9fd72db9c
pableras172 <pableras172@hotmail.com>
2024-04-16 19:11:04 +0200

    Ajuste telefono
    Ajuste telefono

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -26,7 +26,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],            
             'password' => $this->passwordRules(),
-            'telefono' => ['required', 'numeric', 'max:9'],
+            'telefono' => ['required', 'numeric'],
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
             'g-recaptcha-response' => 'required|captcha'            
         ])->validate();
